a4143b7626fcc32eebae261c962c14df44c7131b
Christian Berger <c.berger18@gmail.com>
2024-02-03 12:45:16 +0100

    Updated default spi pin configuration

--- ConnectionConfiguration.cs
+++ ConnectionConfiguration.cs
@@ -15,7 +15,7 @@ public sealed class ConnectionConfiguration
     /// Creates the default ConnectionConfiguration for the selected BusConfiguration
     /// </summary>
     /// <param name="busConfigurationConfiguration">The selected Bus</param>
-    /// <param name="gpioCommunicationPin">The GPIO pin to communicate with the CC1101Controller device</param>
+    /// <param name="gpioCommunicationPin">Any GPIO pin to communicate with the CC1101Controller device</param>
     /// <exception cref="NotImplementedException">Thrown, when the selected Bus has not yet been implemented</exception>
     public ConnectionConfiguration(BusConfiguration busConfigurationConfiguration, int gpioCommunicationPin = 22)
     {
@@ -33,15 +33,15 @@ public sealed class ConnectionConfiguration
     }
     
     /// <summary>
-    /// Creates a ConnectionConfiguration, maps the CC101 Pins to the Raspberry PI 
+    /// Creates a ConnectionConfiguration, maps the CC101 Pins to the Raspberry PI - use the GPIO Pin numbers, not the physical ones!
     /// </summary>
     /// <param name="bus">The Bus ID of the SPI Device</param>
     /// <param name="si">The SI Pin</param>
     /// <param name="so">The SO Pin</param>
     /// <param name="cs">The CS Pin</param>
     /// <param name="sclk">The SCLK Pin</param>
-    /// <param name="gdo2">The CC1101Controller GPIO Communication Pin</param>
-    public ConnectionConfiguration(int bus = 0, int si = 19, int so = 21, int cs = 24, int sclk = 23, int gdo2 = 22)
+    /// <param name="gdo2">Any GPIO pin to communicate with the CC1101Controller device</param>
+    public ConnectionConfiguration(int bus = 0, int si = 10, int so = 9, int cs = 8, int sclk = 11, int gdo2 = 25)
     {
         Bus = bus;
         SI = si;
@@ -70,20 +70,20 @@ public sealed class ConnectionConfiguration
     private void ConfigureForBus0(int gpioCommunicationPin)
     {
         Bus = 0;
-        SI = 19;
-        SO = 21;
-        CS = 24;
-        SCLK = 23;
+        SI = 10;
+        SO = 9;
+        CS = 8;
+        SCLK = 11;
         GDO2 = gpioCommunicationPin;
     }
     
     private void ConfigureForBus1(int gpioCommunicationPin)
     {
         Bus = 1;
-        SI = 38;
-        SO = 35;
-        CS = 36;
-        SCLK = 40;
+        SI = 20;
+        SO = 19;
+        CS = 18;
+        SCLK = 21;
         GDO2 = gpioCommunicationPin;
     }
 
